8e82f7963b44a22ad04db64249ee0a61b4e8986b
Sergey Kilimnik <sergey-ki@btigroup.io>
2020-02-17 13:00:05 +0200

    project created

---
+++ src/MessageProducer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Pulsar.Client.Api;
+using Pulsar.Client.Common;
+
+namespace MessageProducer
+{
+    class Program
+    {
+        static Config LoadConfig()
+        {
+            return JsonConvert.DeserializeObject<Config>(File.ReadAllText("config.json"));
+        }
+        
+        static void Main(string[] args)
+        {
+            var config = LoadConfig();
+            var conn = new PulsarClientBuilder()
+                .ServiceUrl(config.URL)
+                .Build();
+
+            var producer = new ProducerBuilder(conn)
+                .Topic(config.TopicToWrite)
+                .MessageRoutingMode(MessageRoutingMode.SinglePartition)
+                .CompressionType(CompressionType.LZ4)
+                .EnableBatching(false)
+                .CreateAsync()
+                .Result;
+            
+        }
+    }
+}
---
+++ startPulsar.ps1
@@ -0,0 +1,1 @@
+docker run -it -p 6650:6650 -p 8080:8080 --mount source=pulsardata,target=/pulsar/data --mount source=pulsarconf,target=/pulsar/conf apachepulsar/pulsar:2.5.0 bin/pulsar standalone
